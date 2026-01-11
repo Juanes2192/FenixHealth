309550b136666e7e0e1b43f1c8b7321d8e9b59b6
Juanes2192 <juanestebanperdomoleiva@gmail.com>
2026-01-11 10:59:15 -0500

    Se configura react bootstrap

--- client/src/pages/public/Landing.jsx
+++ client/src/pages/public/Landing.jsx
@@ -1,8 +1,32 @@
+import React from "react";
+import {
+  Navbar,
+  Nav,
+  Container,
+  Row,
+  Col,
+  Button,
+  Card,
+  Form
+} from "react-bootstrap";
+
+import {
+  FiBarChart2,
+  FiLayers,
+  FiUsers,
+  FiCheckCircle
+} from "react-icons/fi";
+
+import {
+  FaTasks,
+  FaChartLine
+} from "react-icons/fa";
+
 export default function Landing() {
   return (
-    <main>
-      <h1>FenixHealth</h1>
-      <p>Plataforma inteligente para gestión de salud</p>
-    </main>
+    <>
+     <h1>AMS</h1>
+      <p>Aplicación de Monitoreo deportivo</p>
+    </>
   );
 }
